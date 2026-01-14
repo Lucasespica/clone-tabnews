d2b02520a6b2c9a225f92b221c0ee9e5ac529ca3
Lucasespica <lucasespicarezende@gmail.com>
2026-01-14 14:29:58 -0300

    chore: add `secretlint` and `lint-staged`

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 function Home() {
+  // eslint-disable-next-line no-unused-vars
+  const API_KEY = "AIzaSyA123456789012345678901234567890"; //test secret
   return <h1>Abrace seus pais enquanto eles ainda estão por aqui.</h1>;
 }
 
