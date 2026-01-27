f878724ddcd6faf8aef10168478d4e1759fe1d15
Lucasespica <lucasespicarezende@gmail.com>
2026-01-27 18:53:33 -0300

    feat: add support for `PATCH` on `/api/v1/users/[username]`

--- tests/integration/api/v1/users/post.test.js
+++ tests/integration/api/v1/users/post.test.js
@@ -46,13 +46,12 @@ describe("POST /api/v1/users", () => {
         userInDatabase.password,
       );
 
-      expect(correctPasswordMatch).toBe(true);
-
       const incorrectPasswordMatch = await password.compare(
         "SenhaErrada",
         userInDatabase.password,
       );
 
+      expect(correctPasswordMatch).toBe(true);
       expect(incorrectPasswordMatch).toBe(false);
     });
 
@@ -90,7 +89,7 @@ describe("POST /api/v1/users", () => {
       expect(response2Body).toEqual({
         name: "ValidationError",
         message: "O email informado já está sendo utilizado.",
-        action: "Utilize outro email para realizar o cadastro.",
+        action: "Utilize outro email para realizar esta operação.",
         status_code: 400,
       });
     });
@@ -129,7 +128,7 @@ describe("POST /api/v1/users", () => {
       expect(response2Body).toEqual({
         name: "ValidationError",
         message: "O username informado já está sendo utilizado.",
-        action: "Utilize outro username para realizar o cadastro.",
+        action: "Utilize outro username para realizar esta operação.",
         status_code: 400,
       });
     });
